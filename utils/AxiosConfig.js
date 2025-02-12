d74b1a507ef9bc9a2da7a69ad55f70f25a74c88b
battlesofarmy <battlesofarmy@gmail.com>
2025-02-13 05:38:17 +0600

    backend url changed to live

--- utils/AxiosConfig.js
+++ utils/AxiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // Replace with your API base URL
+  baseURL: "https://muntasir-backend.vercel.app", // Replace with your API base URL
   // baseURL: "https://applications.muntasir3301.xyz", // Replace with your API base URL
   timeout: 10000, // Optional: Set timeout (in ms)
   headers: { "Content-Type": "application/json" }, // Optional: Default headers
