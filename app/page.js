c24d19e349377e5f953fcd3158bd728a2972458a
battlesofarmy <battlesofarmy@gmail.com>
2025-02-14 12:21:46 +0600

    test

--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
-import React from 'react'
 
 export default function page() {
   return (
-    <div>Hello World</div>
+    <>
+      <div className="grid grid-cols-2">
+        <div className="container">
+          <h2>Muntasir Ahmed Fahim</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente fugiat dolore nihil non fuga hic enim odio consequuntur veniam delectus.</p>
+        </div>
+      </div>
+    </>
   )
 }
