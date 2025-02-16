9343ddc42f7e5310cc5cc09ee9531b486d78c79f
battlesofarmy <battlesofarmy@gmail.com>
2025-02-16 18:03:15 +0600

    banner text added

--- app/page.js
+++ app/page.js
@@ -2,12 +2,12 @@
 export default function page() {
   return (
     <>
-      <div className="grid grid-cols-2">
-        <div className="container">
-          <h2>Muntasir Ahmed Fahim vai</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente fugiat dolore nihil non fuga hic enim odio consequuntur veniam delectus.</p>
-        </div>
-      </div>
+        <section className="text-center py-20">
+          <div className="container">
+            <h2 className="text-4xl mb-2">Hi 👋, I am Muntasir Ahmed</h2>
+            <p style={{lineHeight: '1.7rem'}}>I am a running CSE (Bachelors degree at Computer Science and Engineering) Student on 4th year at ZHSUT. I can describe my university life as a balance between academics and extracurricular activities. I am very passionate competitive programmer. I love solving complex problems and breaking them down to simple tasks. I also participate in different hackathons and programming competitions.In terms of technology and framework, I am very comfortable with C++ and Javascript and have done many projects with them. I am also a good team player. Being part of teams I participated and won many programming and development related competitions.I have good leadership skills. In my college life and also in my university life I organized and volunteered in many national level events.My life motto is to be better than yesterday. I try to learn new things everyday. I try to follow a strict routine and punctual. And apart from all this tech stuff I really love traveling. I absolutely love hiking and trekking. And in my free time, I read books or watch movies. I also love to talk with strangers about no particular topic whatsoever.</p>
+          </div>
+        </section>
     </>
   )
 }
